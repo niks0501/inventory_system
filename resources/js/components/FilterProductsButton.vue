24a6ad8c20b0288c863affc52c1b0b5d2ed670c6
Nikko Causapin <nikkocausapin61@gmail.com>
2026-01-26 23:42:41 +0800

    Added a vue component for filtering products by category. Will finish this tomorrow

---
+++ resources/js/components/FilterProductsButton.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+    categories: {
+        type: Array,
+        default: () => [],
+    },
+})
+
+const emit = defineEmits(['update:categories'])
+
+const selected = ref([])
+
+watch(selected, (newValue) => {
+    emit('update:categories', newValue)
+})
+</script>
+
+<template>
+    <div class="filter-wrapper">
+        <button class="filter-btn">
+            Filter Categories
+        </button>
+
+        <div class="filter-dropdown">
+            <label
+                v-for="category in categories"
+                :key="category.id"
+                class="filter-item"
+            >
+                <input
+                    type="checkbox"
+                    :value="category.id"
+                    v-model="selected"
+                />
+                {{ category.name }}
+            </label>
+        </div>
+    </div>
+</template>
